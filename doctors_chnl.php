aa304fe882650edcc39315d0cf9ceefa710bf988
das2497 <115341956+das2497@users.noreply.github.com>
2022-12-04 03:22:28 +0530

    ff

--- doctors_chnl.php
+++ doctors_chnl.php
@@ -4,9 +4,7 @@ session_start();
 
 require 'connection.php';
 
-
 ?>
-
 
 <table class=" table table-responsive shadow stdn mt-4">
     <thead>
@@ -17,14 +15,15 @@ require 'connection.php';
     <tbody>
         <?php
 
-        $rs1 = Database::search("SELECT doctor.id, doctor.uname, doctor.name, doctor.specialty, specialies.speciality, doctor.password FROM doctor INNER JOIN specialies ON doctor.specialty=specialies.id WHERE doctor.specialty='".$_POST["specid"]."';");
+        $rs1 = Database::search("SELECT doctor.id, doctor.uname, doctor.name, doctor.specialty, specialies.speciality, doctor.password FROM doctor INNER JOIN specialies ON doctor.specialty=specialies.id WHERE doctor.specialty='" . $_POST["specid"] . "';");
         $sn = $rs1->num_rows;
 
         for ($i = 0; $i < $sn; $i++) {
             $sd = $rs1->fetch_assoc();
 
         ?>
-            <td><?php echo $sd["name"]; ?></td>
+            <tr class="alert-info">
+                <td><?php echo $sd["name"]; ?></td>
             </tr>
 
         <?php
